780ec21f39890fecd57767eaded57586953d7240
Joaquín Ignacio Ossandón Gómez <joaquinossandong@gmail.com>
2023-07-08 15:41:54 -0400

    Revert "[ADD] prueba Modulo 6 terminada"
    This reverts commit d6de7c5d0cf2702789510ab075bacb894212e9ca.

--- src/helpers/path.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-export const dirname = (importMetaUrl) => {
-  return path.dirname(fileURLToPath(importMetaUrl))
-}
-
-export const join = (...args) => path.join(...args)
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import animeRoute from './anime.route.js'
-
-const router = express.Router()
-
-router.use('/anime', animeRoute)
-
-export default router
